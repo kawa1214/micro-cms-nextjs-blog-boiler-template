500c0a7f3b0dab22da803da878d47686beb838b1
ryo kawamura <kawa1214mura@gmail.com>
2020-12-21 19:32:35 +0900

    fix

--- components/BlogCard/index.tsx
+++ components/BlogCard/index.tsx
@@ -17,21 +17,21 @@ const stripTags = (input: string, allowed: string) => {
 const BlogCard: React.FC<BlogCardProps> = ({ blog }) => {
   return(
     <div className="py-4">
-      <Link href={`/blogs/${blog.id ?? ''}`}>
-        <a className="text-lg font-semibold">{blog.title  ?? ''}</a>
+      <Link href={`/blogs/${blog.id}`}>
+        <a className="text-lg font-semibold">{blog.title}</a>
       </Link>
       <div className="text-sm py-1.5">
-        <Moment format="YYYY-MM-DD" className="text-gray-700 pr-3 font-semibold">{blog.createdAt  ?? ''}</Moment>
+        <Moment format="YYYY-MM-DD" className="text-gray-700 pr-3 font-semibold">{blog.createdAt}</Moment>
         {blog.tags.map(tag => (
-          <div className="inline-flex bg-white mr-2 px-1.5 rounded-md" key={tag.id  ?? ''}>
-            <a className="flex-1 text-gray-700 font-semibold">{tag.name  ?? ''}</a>
+          <div className="inline-flex bg-white mr-2 px-1.5 rounded-md" key={tag.id}>
+            <a className="flex-1 text-gray-700 font-semibold">{tag.name}</a>
           </div>
         ))}
       </div>
       <div
         className="max-h-12 overflow-ellipsis overflow-hidden text-gray-500"
         dangerouslySetInnerHTML={{
-          __html: `${stripTags(blog.body  ?? '', "")}`,
+          __html: `${stripTags(blog.body, "")}`,
         }}
       />
     </div>
--- components/BlogContent/index.tsx
+++ components/BlogContent/index.tsx
@@ -17,11 +17,13 @@ const highlightCode = (body: String) => {
   return $.html()
 }
 
+
+
 const BlogContent: React.FC<BlogContentProps> = ({ blog }) => {
   return (
     <div className={"bg-white rounded-sm"}>
       <div className="rounded-sm grid h-auto w-auto px-4 md:px-12 py-16 md:py-20 bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500">
-        <p className="place-self-center text-center font-bold text-3xl text-white">{blog.title ?? ''}</p>
+        <p className="place-self-center text-center font-bold text-3xl text-white">{blog.title}</p>
       </div>
       <div className="text-black py-6 px-4">
         <div
