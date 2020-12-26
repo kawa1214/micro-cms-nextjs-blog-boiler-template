0c5bf6009d83e158a33338a96b1ccbe6123ac620
ryo kawamura <kawa1214mura@gmail.com>
2020-12-27 01:27:34 +0900

    fix generateSiteMap

--- utils/generateSiteMap.js
+++ utils/generateSiteMap.js
@@ -6,22 +6,19 @@ async function generateSiteMap() {
     //'pages/**/*.tsx',
     //'!pages/**/[id].tsx',
     //'!pages/_*.tsx',
-    'out/blogs/*.html'
+    //'out/blogs/*.html',
+    '.next/server/pages/**/*.html'
   ])
-  
+
   console.log("pages", pages)
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-        <loc>https://kawa.dev</loc>
-      </url>         
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">   
       ${pages
         .map(page => {
           const path = page
-            .replace('pages', '')
-            .replace('out', '')
-            .replace('.tsx', '')
+            .replace('/index', '')
+            .replace('.next/server/pages', '')
             .replace('.html', '')
           return `
       <url>
